3e5443a3c117818b7acfad40e504f0aba037015a
walker.zhang <walker5111@qq.com>
2017-06-21 23:33:38 +0800

    Spring Cloud & Docker, Page 95

---
+++ microservice-consumer-movie-feign-hystrix-fallback/src/main/java/com/flying/learning/ConsumerMovieApplication.java
@@ -0,0 +1,20 @@
+package com.flying.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrix
+public class ConsumerMovieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+}
---
+++ microservice-consumer-movie-feign-hystrix-fallback/src/main/java/com/flying/learning/controller/MovieController.java
@@ -0,0 +1,25 @@
+/*
+ Created by Walker on 2017/6/9.
+ Revision History:
+ Date          Who              Version      What
+ 2017/6/9      Walker           0.1.0        Created. 
+*/
+package com.flying.learning.controller;
+
+import com.flying.learning.entity.User;
+import com.flying.learning.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userFeignClient.findById(id);
+    }
+}
