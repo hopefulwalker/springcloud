6cc2092ebe9b2a5d1dfff84023d34aae466213a3
walker.zhang <walker5111@qq.com>
2017-06-22 22:13:53 +0800

    Spring Cloud & Docker, Page 124

--- microservice-gateway-zuul/src/main/java/com/flying/learning/ZuulApplication.java
+++ microservice-gateway-zuul/src/main/java/com/flying/learning/ZuulApplication.java
@@ -2,6 +2,7 @@ package com.flying.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
--- microservice-hystrix-dashboard/src/main/java/com/flying/learning/HystrixDashboardApplication.java
+++ microservice-hystrix-dashboard/src/main/java/com/flying/learning/HystrixDashboardApplication.java
@@ -2,6 +2,7 @@ package com.flying.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
